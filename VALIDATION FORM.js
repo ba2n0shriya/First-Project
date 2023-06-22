86f13a0a24d3f2382cb5794dd8ee80795b041f5f
ba2n0shriya <shriyasingh1100@gmail.com>
2023-06-22 16:55:47 +0530

    First Project

--- VALIDATION FORM.js
+++ VALIDATION FORM.js
@@ -1,77 +1,40 @@
-// let myform = document.getElementById('my-form');
-// let submitButton = document.getElementById('submit-button');
-// let nameE1= document.getElementById('name');
-// let emailE1 = document.getElementById('email');
-// let passwordE1 = document.getElementById('password');
-// let genderE1 = document.getElementById('gender');
+let myform = document.getElementById('my-form');
+let nameE1 = document.getElementById('name');
+let emailE1 = document.getElementById('email');
+let passwordE1 = document.getElementById('password');
+let genderE1 = document.getElementById('gender');
+let submitbutton = document.getElementById('submit-button');
 
-// myform.addEventListener('submit', function (event){
-//     event.preventDefault();
+myform.addEventListener('submit', function(event) {
+    event.preventDefault();
+});
 
-// let name = nameE1.value ;
-// let email = emailE1.value ;
-// let password= passwordE1.value ;
-// let gender = genderE1.value ;
+let formdata = {
+    name: '',
+    email: '',
+    password: '',
+    gender: ''
+};
 
-// let formdata = {}
-//     name:nameE1.''
-//     email:emailE1.''
-//     password:passwordE1.''
-//     gender:genderE1.''
-// };
-// console.log(formdata);
+nameE1.addEventListener('input', function() {
+    formdata.name = nameE1.value
+});
 
-// });
-// nameE1.addEventListener('input')
+emailE1.addEventListener('input', function() {
+    formdata.email = emailE1.value
+});
 
-// let myform = document.getElementById('my-form');
-// let nameE1 = document.getElementById('name');
-// let emailE1 = document.getElementById('email');
-// let passwordE1 = document.getElementById('password');
-// let genderE1 = document.getElementById('gender');
-// let submitButton = document.getElementById('submit-button');
+passwordE1.addEventListener('input', function() {
+    formdata.password = passwordE1.value;
+});
 
-// myform.addEventListener('submit', function(event) {
-//     event.preventDefault();
-
-// });
-
-//     let formdata = {
-
-//     name: '',
-//     email:'',
-//     password:'',
-//     gender:'',
-//     };
+genderE1.addEventListener('change', function() {
+    formdata.password = passwordE1.value;
+});
 
 
-// nameE1.addEventListener('input', function() {
-//     formdata.name = nameE1.value;
-// });
 
-// emailE1.addEventListener('input', function() {
-//     formdata.email = emailE1.value;
-// });
 
-// passwordE1.addEventListener('input', function() {
-//     formdata.password = passwordE1.value;
-// });
-
-// genderE1.addEventListener('change', function() {
-//     formdata.gender = genderE1.value;
-// });
-
-// submitbutton.onclick = function(){
-//     console.log(formdata);
-// }
-function greeting(display){
-    console.log("hello")
-    display()
-    console.log("how are you")
+submitbutton.onclick = function() {
+    console.log(formdata)
 }
-greeting(function(){
-    console.log("shriya")
-})
-
-
-
